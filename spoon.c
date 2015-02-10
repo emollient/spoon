9b7e39da24f37a7b54ea26ac96519f021512251a
susan <slunn@csh.rit.edu>
2015-02-10 11:03:47 -0500

    main function

---
+++ spoon.c
@@ -0,0 +1,13 @@
+/**
+ * spoon.c - the main function for spoon, a super duper powerful utensil
+ * @author Susan Lunn
+ * @date 02/07/2015
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+
+}
